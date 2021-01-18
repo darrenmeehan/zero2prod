827d21f72e0e870c03f4dcf7fdac80bd591ff300
Darren Meehan <hi@drn.ie>
2021-01-18 11:28:37 +0000

    Use port 8000 in application

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     // We retrieve the port assigned to use by the OS
     run(listener)?.await
 }
