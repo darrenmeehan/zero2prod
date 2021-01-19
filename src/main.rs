cc43f826898b5a4fc0c676ff5f3713e592e800b1
Darren Meehan <hi@drn.ie>
2021-01-19 14:36:40 +0000

    Add actix-web's logs to console

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
