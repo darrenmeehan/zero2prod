139d8222b704cce3585ee6337ae03fedbf0c62ab
Darren Meehan <hi@drn.ie>
2021-01-19 19:05:41 +0000

    Add app logs to traces

--- src/startup.rs
+++ src/startup.rs
@@ -4,13 +4,14 @@ use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger)
             .route("health_check", web::get().to(health_check))
             .route("subscriptions", web::post().to(subscribe))
             // Our pool is already wrapped in a Data:
