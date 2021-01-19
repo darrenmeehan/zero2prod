a374956fcc3025492cd155d81860dd98c92b7473
Darren Meehan <hi@drn.ie>
2021-01-19 19:06:09 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
 
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,6 +1,7 @@
 use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer}; use tracing_log::LogTracer;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 /// Compose multiple layers into  a `tracing`'s subscriber.
@@ -8,15 +9,12 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 /// # Implementation Notes
 ///
 /// We are using `impl Subscriber` as return type to avoid having to
-/// spell out the actual type of the returned subscriber, which is 
+/// spell out the actual type of the returned subscriber, which is
 /// indeed quite complex.
 /// We need to explicitly call out that the returned subscriber is
 /// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
 /// later on.
-pub fn get_subscriber(
-    name: String,
-    env_filter: String
-) -> impl Subscriber + Send + Sync {
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
