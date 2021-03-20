1a5e2878a3d345a219e7ffcdd3fbe20d9d4bda25
Darren Meehan <hi@drn.ie>
2021-03-20 18:43:24 +0000

    Remove comment

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -2,7 +2,6 @@ use crate::domain::subscriber_email::SubscriberEmail;
 use crate::domain::subscriber_name::SubscriberName;
 
 pub struct NewSubscriber {
-    // We are not using `String` anymore!
     pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
